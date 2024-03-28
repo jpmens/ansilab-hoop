757914d8beefce0a59244036b7aeb32341687093
Jan-Piet Mens <jp@mens.de>
2024-03-28 10:13:02 +0100

    add rot13 filter

--- plugins/filter/rot13.py
+++ plugins/filter/rot13.py
@@ -1,23 +1,41 @@
+DOCUMENTATION = r'''
+  name: rot13
+  version_added: "1.0.2"
+  short_description: encode/decode to ROT13
+  description:
+    - Encode or decode a string to ROT13
+  positional: _input, query
+  options:
+    _input:
+      description: String to encode or decode.
+      type: str
+      required: true
+'''
+
+EXAMPLES = r'''
+
+    encoded: '{{ "Ansible" | rot13 }}'
+    # => "Nafvoyr"
+
+    decoded: '{{ "Nafvoyr" | rot13 }}'
+    # => "Ansible"
+'''
+
+RETURN = r'''
+  _value:
+    description:
+      - A string with encoded or decoded ROT13 content
+    type: string
+'''
 import codecs
 
 # ROT13 of string `s' 
 def rot13(s):
     return codecs.encode(s, 'rot_13')
 
-# Word Count of `s'; `cl' is class: 'chars', 'words', or 'lines'
-def wc(s, cl='c'):
-    
-    if cl.startswith("l"):
-        return s.count('\n')
-    elif cl.startswith("w"):
-        return len(s.split())
-    return len(s)
-
 class FilterModule(object):
 
     def filters(self):
         return {
             'rot13'     : rot13,
-            'wc'        : wc,
-            'wordcount' : wc,
         }
